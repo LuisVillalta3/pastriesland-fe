ad9d6f9696f3dfb3467d5bd7c491e85e673cef39
Villalta <luisvillalta3@gmail.com>
2025-06-01 17:23:19 -0600

    feat: client / home page

---
+++ src/app/client/client.route.ts
@@ -0,0 +1,14 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from '@client/pages/home/home.component';
+import {MainLayoutComponent} from '@client/layouts/main-layout/main-layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'inicio', redirectTo: '', pathMatch: 'full' },
+      { path: '', component: HomeComponent }
+    ],
+  }
+]
