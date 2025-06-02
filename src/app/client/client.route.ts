f14819ca151b1b703c4198fae4f5022d7df50abb
Villalta <luisvillalta3@gmail.com>
2025-06-01 18:02:23 -0600

    feat: client / sobre nosotros page

--- src/app/client/client.route.ts
+++ src/app/client/client.route.ts
@@ -1,14 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from '@client/pages/home/home.component';
 import {MainLayoutComponent} from '@client/layouts/main-layout/main-layout.component';
+import {SobreNosotrosComponent} from '@client/pages/sobre-nosotros/sobre-nosotros.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
+      { path: 'sobre-nosotros', component: SobreNosotrosComponent },
       { path: 'inicio', redirectTo: '', pathMatch: 'full' },
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
     ],
   }
 ]
